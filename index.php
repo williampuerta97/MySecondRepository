f67da80c30af0c8b05aebd909c2e14c00dbd04d3
William Puerta <williampuerta1097@gmail.com>
2017-11-21 07:51:55 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GIT&GITHUB</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>GIT - GITHUB</h1>
+	<p>Creando un repositorio para cargarlo a github</p>
+</body>
+</html>
